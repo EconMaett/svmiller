e5eb0279c7d123f8827e2bdd14418cb37eb329d0
EconMaett <matthias.spichiger@bluewin.ch>
2024-03-13 14:43:55 +0100

    Rmd

--- R/01_inflation-adjustment.R
+++ R/01_inflation-adjustment.R
@@ -1,4 +1,6 @@
 # 01 - How to Adjust for Economic Indicators for Inflation (and Index Them) ----
-# URL: http://svmiller.com/blog/2023/01/index-economic-data-adjust-inflation/
+# Blogpost: http://svmiller.com/blog/2023/01/index-economic-data-adjust-inflation/
+# RMarkdown: https://github.com/svmiller/svmiller.github.io/blob/master/_rmd/2023-01-19-index-economic-data-adjust-inflation.Rmd
+
 
 
